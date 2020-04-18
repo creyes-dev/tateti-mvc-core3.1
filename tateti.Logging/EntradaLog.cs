2e148c8cca70d359cd2bfcdb9b7836454c524e69
Cristian <creyes.dev@gmail.com>
2020-04-18 00:16:32 -0300

    Logging
    Agregado nuevo proyecto de logging

---
+++ tateti.Logging/EntradaLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tateti.Logging
+{
+    public class EntradaLog
+    {
+        public int IdEvento { get; internal set; }
+        public string Mensaje { get; internal set; }
+
+        public string NivelLog { get; internal set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
---
+++ tateti.Logging/LoggingArchivoManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace tateti.Logging
+{
+    public class LoggingArchivoManager
+    {
+        private string _nombreArchivo;
+
+        public LoggingArchivoManager(string nombreArchivo)
+        {
+            _nombreArchivo = nombreArchivo;
+        }
+
+        static ReaderWriterLock locker = new ReaderWriterLock();
+
+        public void InsertarLog(EntradaLog entradaLog)
+        {
+            var directorio = System.IO.Path.GetDirectoryName(_nombreArchivo);
+
+            if (!System.IO.Directory.Exists(directorio))
+                System.IO.Directory.CreateDirectory(directorio);
+
+            try
+            {
+                locker.AcquireWriterLock(int.MaxValue);
+                System.IO.File.AppendAllText(_nombreArchivo, $"{entradaLog.IdEvento} {entradaLog.NivelLog} {entradaLog.Mensaje}" + Environment.NewLine);
+            }
+            finally
+            {
+                locker.ReleaseWriterLock();
+            }
+        }
+
+    }
+}
